23b63ef5e7f6f76b5e7883c3686ba61db2f09429
umutavin <umutavin@gmail.com>
2018-02-28 22:36:53 +0300

    Completed

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resetButton: UIButton!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var pauseButton: UIButton!
+    @IBOutlet var counterText: UILabel!
+    var counter = 0
+    var isPlaying = false
+    var timer = Timer()
+    
     override func viewDidLoad() {
+        counterText.text = String(counter)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+   
+    @IBAction func resetTouch(_ sender: Any) {
+        timer.invalidate()
+        isPlaying=false
+        counter = 0;
+        counterText.text = String(counter)
+        playButton.isEnabled = true
+        pauseButton.isEnabled = true
+        
+    }
+    
+    @IBAction func playTouch(_ sender: Any) {
+        isPlaying=true
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updCounter), userInfo: nil, repeats: true)
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        
+    }
+    
+    @IBAction func pauseTouch(_ sender: Any) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        timer.invalidate()
+        isPlaying=false
+        
+    }
+    
+    
+    @objc func updCounter(){
+        counter = counter + 1
+        counterText.text = String(counter)
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
